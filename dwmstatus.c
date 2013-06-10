48f608b0949756b0ded7cf2c5dba9b74ff9a00a9
albanb <binmail@free.fr>
2013-06-10 21:15:10 +0200

    Clean a little bit

--- dwmstatus.c
+++ dwmstatus.c
@@ -8,70 +8,7 @@
 #include <X11/Xlib.h>
 #include <alsa/asoundlib.h>
 #include <dirent.h>
-
-#define BATT_LOW		15			// Below BATT_LOW percentage left on battery, the battery display turns red
-#define INTERVAL		1			// Sleeps for INTERVAL seconds between updates
-#define CPU_HI			80			// Above CPU_HIGH, cpu display turns red
-#define	WIRELESS		"wlp3s0"
-#define WIRED			"enp10s0"
-
-/* Files to read system information */
-#define BATT_NOW		"/sys/class/power_supply/BAT0/charge_now"
-#define BATT_FULL		"/sys/class/power_supply/BAT0/charge_full"
-#define BATT_STAT		"/sys/class/power_supply/BAT0/status"
-#define CPU_FILE		"/proc/stat"
-#define MEM_FILE		"/proc/meminfo"
-#define FILE_UPLOAD		"/sys/class/net/%s/statistics/tx_bytes"
-#define FILE_DOWNLOAD		"/sys/class/net/%s/statistics/rx_bytes"
-#define FILE_OPER		"/sys/class/net/%s/operstate"
-//#define NET_FILE_UPLOAD	"/sys/class/net/enp10s0/statistics/tx_bytes"
-//#define NET_FILE_DOWNLOAD	"/sys/class/net/enp10s0/statistics/rx_bytes"
-//#define NET_FILE_OPER		"/sys/class/net/enp10s0/operstate"
-//#define WIFI_FILE_UPLOAD	"/sys/class/net/wlp3s0/statistics/tx_bytes"
-//#define WIFI_FILE_DOWNLOAD	"/sys/class/net/wlp3s0/statistics/rx_bytes"
-//#define WIFI_FILE_OPER	"/sys/class/net/wlp3s0/operstate"
-#define TODO_FILE		"/home/alban/.local/share/todo/todo.txt"
-#define MOUNT_DIR		"/media"
-#define KERNELOS		"/proc/sys/kernel/osrelease"
-#define NOTIFILE		"/home/alban/.local/share/dwm/notification"
-
-#define VOL_CH			"Master"	// Channel to watch for volume
-
-/* Strings format for information display in the status bar */
-#define VOL_STR			"\x01\uE05D %d%%"			// Volume
-#define VOL_MUTE_STR		"\x05\uE04F %d%%"			// Volume
-#define BAT_STR			"\x04\uE032 %d%%"			// Battery, BAT, above BATT_LOW percentage
-#define BAT_LOW_STR		"\x03\uE031 %d%%"			// Battery, BAT, below BATT_LOW percentage
-#define BAT_FULL_STR		"\x01\uE033 %d%%"			// Battery, full
-#define BAT_CHRG_STR		"\x05\uE032 %d%%"			// Battery, AC
-#define CPU_STR			"\x01\uE026 %d%%"			// CPU percent
-#define CPU_HI_STR		"\x03\uE026 %d%%"			// CPU percent when above CPU_HI%
-#define MEM_STR			"\x01\uE020 %ld%ciB/%ld%ciB"		// MEM percent
-#define MEM_SWAP_STR		"\x04\uE020 %ld%ciB/%ld%ciB"		// MEM percent
-#define NET_STR			"\x01\uE060 %ldKbp/s \uE061 %ldKbp/s"	//NET oper string up and down
-#define WIFI_STR		"\x01\uE02D \uE060 %ldKbp/s \uE061 %ldKbp/s"	//NET down string up and down
-#define NET_DOWN_STR		"\x03\uE060 %ldKbp/s \uE061 %ldKbp/s"	//NET down string up and down
-#define DATE_TIME_STR		"\x01\uE015 %a %d %b %Y %H:%M"		// This is a strftime format string which is passed localtime
-#define TODO_STR		"\x01\uE01F %d"			// Todo string
-#define MOUNT_STR		"\x01\uE00C %d"			// Mount string
-#define KERNEL_STR		"\x01\uE00E %s"			// Kernel string
-#define TORRENT_STR		"\x01\uE065 %d"			// Torrent string
-#define REMIND_STR		"\x01\uE01C %d"			// Reminder string
-
-/* Macros definition */
-#define LENGTH(X) (sizeof X / sizeof X[0])
-
-/* structures declaration*/
-typedef struct {
-	int (*func)(char *);
-	int refresh;
-	char * const *cmd;
-} Stbar;
-
-typedef struct {
-	char *cmp;
-	char *chaine;
-} Notification;
+#include <dwmstatus.h>
 
 /* Functions declaration */
 static int alsa_sound(char *stat);
@@ -85,10 +22,7 @@ static int todo(char *stat);
 static int mount(char *stat);
 static int os_kernel(char *stat);
 static int notifications(char *stat);
-//static void spawn(char * const *arg);
 static int runevery(time_t *ltime, int sec);
-
-//static char * const video[]  = { "vlc", NULL };
 
 static Stbar stbar[] = {
 	{ notifications, 60, NULL },
@@ -107,6 +41,7 @@ static Notification notification[] = {
 	{"torrent;", TORRENT_STR },
 	{"cal;", REMIND_STR },
 };
+
 /*Main function*/
 int main() {
 	Display *dpy;
@@ -126,7 +61,7 @@ int main() {
 	}
 	root = XRootWindow(dpy,DefaultScreen(dpy));
 /* MAIN LOOP STARTS HERE */
-	for (;;) {
+	while (1) {
 		statnext[0] = '\0';
 		status[0] = '\0';
 
@@ -202,7 +137,7 @@ int alsa_sound(char *stat)
 	snd_mixer_selem_get_playback_switch(mas_mixer, SND_MIXER_SCHN_MONO, &mute); /* get mute state */
 
 	if(mute == 0)
-		len = sprintf(stat, VOL_MUTE_STR, 10);
+		len = sprintf(stat, VOL_MUTE_STR, 0);
 	else
 		realvol = (vol*100)/max;
 		len = sprintf(stat, VOL_STR, realvol);
@@ -360,85 +295,6 @@ int net(char *stat)
 	return len;
 }	
 
-/*
-int net(char *stat)
-{
-	int len;
-	FILE *infile;
-	static long slup=0,sldown=0;
-	long lnum1,lnum2,lnum1net,lnum2net,lnum1wifi,lnum2wifi,luploadnet,ldownloadnet,luploadwifi,ldownloadwifi;
-	char netstattmp[50],wifistattmp[50];
-
-	infile=fopen(NET_FILE_UPLOAD,"r");
-	if (infile != NULL)
-	{
-		fscanf(infile,"%ld\n",&lnum1net);
-		fclose(infile);
-		infile=fopen(NET_FILE_DOWNLOAD,"r");
-		fscanf(infile,"%ld\n",&lnum2net);
-		fclose(infile);
-		infile=fopen(NET_FILE_OPER,"r");
-		fscanf(infile,"%s\n",netstattmp);
-		fclose(infile);
-		luploadnet=(lnum1net-slup)/512;
-		ldownloadnet=(lnum2net-sldown)/512;
-	}
-	else
-	{
-		netstattmp[0] ='\0';
-	}
-
-	infile=fopen(WIFI_FILE_OPER,"r");
-	if (infile != NULL)
-	{
-		fscanf(infile,"%s\n",wifistattmp);
-		fclose(infile);
-		infile=fopen(WIFI_FILE_UPLOAD,"r");
-		fscanf(infile,"%ld\n",&lnum1wifi);
-		fclose(infile);
-		infile=fopen(WIFI_FILE_DOWNLOAD,"r");
-		fscanf(infile,"%ld\n",&lnum2wifi);
-		fclose(infile);
-		luploadwifi=(lnum1wifi-slup)/512;
-		ldownloadwifi=(lnum2wifi-sldown)/512;
-	}
-	else
-	{
-		wifistattmp[0]='\0';
-	}
-
-	if ((strncmp(netstattmp,"down",4) == 0) && (strncmp(wifistattmp,"down",4) == 0))
-	{
-		lnum1=0;
-		lnum2=0;
-		len=sprintf(stat,NET_DOWN_STR,luploadnet,ldownloadnet);
-	}
-	else if (strncmp(netstattmp,"up",2) == 0)
-	{
-		lnum1=lnum1net;
-		lnum2=lnum2net;
-		len=sprintf(stat,NET_STR,luploadnet,ldownloadnet);
-	}
-	else if (strncmp(wifistattmp,"up",2) == 0)
-	{
-		lnum1=lnum1wifi;
-		lnum2=lnum2wifi;
-		len=sprintf(stat,WIFI_STR,luploadwifi,ldownloadwifi);
-	}
-	else
-	{
-		lnum1=0;
-		lnum2=0;
-		len=sprintf(stat,NET_DOWN_STR,luploadnet,ldownloadnet);
-	}
-
-	slup=lnum1;
-	sldown=lnum2;
-
-	return len;
-}
-*/
-
 /* Set todo number */
 int todo(char *stat)
 {
@@ -490,15 +346,6 @@ int mount(char *stat)
 
 	return len;
 }
-
-/*
-void spawn(char * const *arg) {
-	if(fork() == 0) {
-		setsid();
-		execvp(arg[0], arg);
-		fprintf(stderr, "dwm: execvp %s", arg[0]);
-	}
-}*/
 
 /* Release number */
 int os_kernel(char *stat)
