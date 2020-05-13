56968abe7ed69885a7af299bd1af770c0dec90cf
albanb <binmail@free.fr>
2020-05-13 22:38:49 +0200

    Test with a new color modifier

--- dwmstbar.h
+++ dwmstbar.h
@@ -25,31 +25,31 @@
 #define CPU_HI			80			// Above CPU_HIGH, cpu display turns red
 
 /* Strings format for information display in the status bar */
-#define VOL_STR			"\x02\uE05D \x01%d%% "			// Volume
-#define VOL_MUTE_STR		"\x03\uE04F %d%%i "			// Volume
-#define BAT_STR			"\x04\uE032 %dmin "			// Battery, BAT, above BATT_LOW percentage
-#define BAT_LOW_STR		"\x03\uE031 %dmin "			// Battery, BAT, below BATT_LOW percentage
-#define BAT_FULL_STR		"\x02\uE033 \x01%d%% "			// Battery, full
-#define BAT_CHRG_STR		"\x02\uE032 \x01%ld%% "			// Battery, AC
-#define CPU_STR			"\x02\uE026 \x01%d%% "			// CPU percent
-#define CPU_HI_STR		"\x03\uE026 %d%% "			// CPU percent when above CPU_HI%
-#define MEM_STR			"\x02\uE020 \x01%ld%ciB "		// MEM percent
-#define MEM_SWAP_STR		"\x04\uE020 %ld%ciB "		// MEM percent
-#define NET_STR			"\x02\uE060 \x01%ldKbp/s \x02\uE061 \x01%ldKbp/s "	//NET oper string up and down
-#define WIFI_STR		"\x02\uE02D \uE060 \x01%ldKbp/s \x02\uE061 \x01%ldKbp/s "	//NET down string up and down
-#define NET_DOWN_STR		"\x03\uE060 %ldKbp/s \uE061 %ldKbp/s "	//NET down string up and down
-#define DATE_TIME_STR		"\x02\uE015 \x01%a %d %b %Y %H:%M"		// This is a strftime format string which is passed localtime
-#define TEMP_STR		"\x02\uE01D \x01%d°C "			// Temperature
-#define SIZE_STR		"\x02\uE077 \x01/%d%% \uE10E %d%% "			// Size
-#define TASK_STR		"\x02\uE01F \x01%c "			// Task string
-#define MOUNT_STR		"\x02\uE00C \x01%d "			// Mount string
-#define KERNEL_STR		"\x02\uE00E \x01%s "			// Kernel string
-#define MAIL_STR_0		"\x02\uE072 "		// Mail notification string
-#define MAIL_STR_D		"\x03\uE072 "		// Mail notification string when no mail check
-#define MAIL_STR_1		"\x01y%d "			// Mail notification string
-#define MAIL_STR_2		"\x01 s%d "			// Mail notification string
-#define MAIL_STR_3		"\x01 f%d "			// Mail notification string
-#define MAIL_STR_4		"\x01 g%d "			// Mail notification string
+#define VOL_STR			"[2]\uE05D[1]%d%%"			// Volume
+#define VOL_MUTE_STR		"[3]\uE04F %d%%"			// Volume
+#define BAT_STR			"[4]\uE032 %dmin"			// Battery, BAT, above BATT_LOW percentage
+#define BAT_LOW_STR		"[3]\uE031 %dmin"			// Battery, BAT, below BATT_LOW percentage
+#define BAT_FULL_STR		"[2]\uE033[1]%d%%"			// Battery, full
+#define BAT_CHRG_STR		"[5]\uE032 %ld%%"			// Battery, AC
+#define CPU_STR			"[2]\uE026[1]%d%%"			// CPU percent
+#define CPU_HI_STR		"[3]\uE026 %d%%"			// CPU percent when above CPU_HI%
+#define MEM_STR			"[2]\uE020[1]%ld%ciB"		// MEM percent
+#define MEM_SWAP_STR		"[4]\uE020 %ld%ciB"		// MEM percent
+#define NET_STR			"[2]\uE060[1]%ldKbp/s[2]\uE061[1]%ldKbp/s"	//NET oper string up and down
+#define WIFI_STR		"[2]\uE02D \uE060[1]%ldKbp/s[2]\uE061[1]%ldKbp/s"	//NET down string up and down
+#define NET_DOWN_STR		"[3]\uE060 %ldKbp/s \uE061 %ldKbp/s"	//NET down string up and down
+#define DATE_TIME_STR		"[2]\uE015[1]%a %d %b %Y %H:%M"		// This is a strftime format string which is passed localtime
+#define TEMP_STR		"[2]\uE01D[1]%d°C"			// Temperature
+#define SIZE_STR		"[2]\uE077[1]/ %d%% \uE10e %d%%"			// Size
+#define TASK_STR		"[2]\uE01F[1]%c"			// Task string
+#define MOUNT_STR		"[2]\uE00C[1]%d"			// Mount string
+#define KERNEL_STR		"[2]\uE00E[1]%s"			// Kernel string
+#define MAIL_STR_0		"[2]\uE072"			// Mail notification string
+#define MAIL_STR_D		"[3]\uE072 "		// Mail notification string when no mail check
+#define MAIL_STR_1		"[1] y%d"			// Mail notification string
+#define MAIL_STR_2		"[1] s%d"			// Mail notification string
+#define MAIL_STR_3		"[1] f%d"			// Mail notification string
+#define MAIL_STR_4		"[1] g%d"			// Mail notification string
 
 static Stbar stbar[] = {
 	{ mount, 1, NULL },
